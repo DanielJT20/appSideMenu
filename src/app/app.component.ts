592eb73765be4227edb9fca305541cfd7714a729
danielJT20 <nielteixeira2022@gmail.com>
2024-01-10 14:04:14 -0300

    teste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,14 @@ export class AppComponent {
   public appPages = [
     { title: 'Home', url: '/folder/home', icon: 'home' },
     { title: 'Produtos', url: '/produtos', icon: 'bag-handle' },
-    { title: 'Cadastrar Produtos', url: '/cad-produtos', icon: 'bag-add' },
     { title: 'Promoções', url: '/folder/promocoes', icon: 'pricetags' },
     { title: 'Lojas', url: '/folder/lojas', icon: 'storefront' },
     { title: 'Perfil', url: '/folder/perfil', icon: 'person-circle' },
     { title: 'Contato', url: '/folder/contato', icon: 'call' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+
+  public adminPages = [
+    { title: 'Cadastrar Produtos', url: '/cad-produtos', icon: 'bag-add' },
+  ];
   constructor() {}
 }
